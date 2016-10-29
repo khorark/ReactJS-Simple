70f21eb892a6e065072f33560e3c2b82249c9495
khorark <khorark@mail.ru>
2016-10-29 21:55:37 +0800

    Добавлено состояние count

--- static/app.jsx
+++ static/app.jsx
@@ -2,21 +2,6 @@
  * Created by arkadiy on 29.10.16.
  */
 
-// function Hero(props) {
-//     let count = 0;
-//     function handleClick() {
-//         count += 1;
-//     }
-//     return (
-//         <div className="container">
-//             <div className="count">{count}</div>
-//             <img src={props.imageUrl} onClick={ handleClick }/>
-//             <h1>{props.title}</h1>
-//             <p>{props.subtitle}</p>
-//         </div>
-//     );
-// }
-
 let Hero = React.createClass({
     getInitialState: function () {
         return {
@@ -24,8 +9,10 @@ let Hero = React.createClass({
         };
     },
 
-    handlerClick: function () {
-        this.state.count += 1;
+    handleClick: function () {
+        this.setState({
+            count: this.state.count + 1
+        });
     },
 
     render: function () {
